a75727c7f686afbcfc2e69c2740a1c697d7024e5
Amarachukwu Onuoha <amarachukwuonuohabars007@gmail.com>
2026-04-20 00:08:02 +0100

    pleaase

--- models/Tasks.ts
+++ models/Tasks.ts
@@ -2,7 +2,7 @@ import mongoose, { Schema, model, models } from 'mongoose';
 
 const TaskSchema = new Schema({
   title: { type: String, required: true },
-  type: { type: String, enum: ['video', 'link'], required: true },
+  type: { type: String, enum: ['video', 'link','url'], required: true },
   url: { type: String, required: true }, // YouTube URL or Website Link
   creatorId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, // Who created the ad
   status: { type: String, default: 'Pending Review' }, // 'Active' or 'Pending Review'
